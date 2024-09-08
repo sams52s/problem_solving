84348003863a06f10430195438f4b238f1643182
T52Sams <sams52tas@gmail.com>
2024-09-08 07:55:37 +0600

    update

--- LeetCode_problems_solution/src/javaProblemsSolution/LinkedListinBinaryTree.java
+++ LeetCode_problems_solution/src/javaProblemsSolution/LinkedListinBinaryTree.java
@@ -3,22 +3,22 @@ package javaProblemsSolution;
 import java.util.HashSet;
 
 public class LinkedListinBinaryTree {
-    public boolean isSubPath(ListNode head, TreeNode root) {
-        return helper(head,head,root);
-
-    }
-    boolean helper(ListNode head, ListNode cur, TreeNode root){
-        if(cur == null)
-            return true;
-        if(root == null)
-            return false;
-
-        if(cur.val == root.val)
-            cur = cur.next;
-        else if(head.val == root.val)
-            head = head.next;
-        else cur = head;
-
-        return helper(head,cur,root.left) ||  helper(head,cur,root.right);
-    }
+//    public boolean isSubPath(ListNode head, TreeNode root) {
+//        return helper(head,head,root);
+//
+//    }
+//    boolean helper(ListNode head, ListNode cur, TreeNode root){
+//        if(cur == null)
+//            return true;
+//        if(root == null)
+//            return false;
+//
+//        if(cur.val == root.val)
+//            cur = cur.next;
+//        else if(head.val == root.val)
+//            head = head.next;
+//        else cur = head;
+//
+//        return helper(head,cur,root.left) ||  helper(head,cur,root.right);
+//    }
 }

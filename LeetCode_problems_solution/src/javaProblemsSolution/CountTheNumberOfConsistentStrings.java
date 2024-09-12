063dbb57cbdf24d55470addf79918eec782ca2ee
Sams, A.B.M. Toufiqul Alam <sams52tas@gmail.com>
2024-09-12 19:46:02 +0600

    solution of Count the Number of Consistent Strings

---
+++ LeetCode_problems_solution/src/javaProblemsSolution/CountTheNumberOfConsistentStrings.java
@@ -0,0 +1,20 @@
+class CountTheNumberOfConsistentStrings {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int consistent = 0;
+        HashSet<Character> set = new HashSet<>();
+        for(int i=0; i<allowed.length(); i++){
+            set.add(allowed.charAt(i));
+        }
+        for(int i=0; i<words.length; i++){
+            String str = words[i];
+            consistent++;
+            for(int j=0; j<str.length(); j++){
+                if(!set.contains(str.charAt(j))){
+                    consistent--;
+                    break;
+                }
+            }
+        }
+        return consistent;
+    }
+}

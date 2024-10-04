6dceb97bd08400a9f6bd40ccf5d3cf0a6d575fe7
Sams, A.B.M. Toufiqul Alam <sams52tas@gmail.com>
2024-10-04 23:46:09 +0600

    Create DividePlayersIntoTeamsofEqualSkill.java

---
+++ LeetCode_problems_solution/src/javaProblemsSolution/DividePlayersIntoTeamsofEqualSkill.java
@@ -0,0 +1,14 @@
+class DividePlayersIntoTeamsofEqualSkill {
+    public long dividePlayers(int[] skill) {
+        Arrays.sort(skill);
+        int tempSub = skill[0] + skill[skill.length - 1];
+        long result = 0;
+        for (int i = 0; i < skill.length/2; i++) {
+           if (skill[i] + skill[skill.length - i - 1] != tempSub) {
+                return -1;
+            }
+             result += (long) skill[i] * skill[skill.length - i - 1];
+        }
+        return result;
+    }
+}
